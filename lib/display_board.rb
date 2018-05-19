3f6bea12e7d8c069531590dfa8bb6c0db74a81e6
Jason Pitts <juniorjr083@gmail.com>
2018-05-19 01:03:26 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,15 @@
 # out the current state.
 
 def display_board(board)
-  if board == [] || board == nil
+ 
  board.push("   |   |   ", "-----------", "   |   |   ","-----------","   |   |   ")
   puts board
+
+
+
+
+
+
 end
  
  
